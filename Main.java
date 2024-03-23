1978cb6bd9ec3a1a75a22fb809ea04d646c6b3ae
shubham-55 <shubham14anand@gmail.com>
2024-03-23 23:15:18 +0530

    third commit

--- Main.java
+++ Main.java
@@ -6,5 +6,6 @@ public class Main{
         System.out.println("output from  java progrsm");
         System.out.println(new Date());
         System.out.println("git remote add origin https://github.com/shubham-55/jenkinPractice.git");
+        System.out.println("third commit");
     }
 }
